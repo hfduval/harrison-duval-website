1ca8ba78a956f0fa1ca0f6090b3d34c6a3a9111d
hfduval <hfduval@gmail.com>
2023-09-26 20:07:38 -0500

    Fix about page padding for mobile

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,7 +5,7 @@ export default function About() {
     return (
         <Layout>
             <main className="justify-between">
-                <div className="flex min-h-screen flex-col pl-8 pt-24 pr-8">
+                <div className="flex min-h-screen flex-col pl-8 pt-24 pr-8 pb-8">
                     <h1 className="mb-2 text-xl font-semibold">Intro</h1>
                     <h2>{strings.about}</h2>
                     <h1 className="mt-16 mb-2 text-xl font-semibold">Hobbies</h1>
